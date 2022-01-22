20ade77609eb424eb7c9c5df382a537afd262e1e
Zener_085 <57841939+Zener085@users.noreply.github.com>
2022-01-22 08:43:04 +0300

    Create 1 module for Bots' course

---
+++ Bots/Module 1/Variables.py
@@ -0,0 +1,31 @@
+a = int(1)  # 1
+b = float(1)  # 1.0
+c = str(1)  # '1'
+d = bool(1)  # True
+
+f = int(b)  # 1
+b = float(a)  # 1.0
+
+print(a + b)  # 2.0
+
+print(a * b)  # 1.0
+
+print(a / b)  # 1.0
+print(-1 / b)  # -1.0
+print(1 / 3)  # 0.3333...
+
+print(a // b)  # 1
+print(-1 // b)  # -1
+
+print(a % b)  # 0
+print(-1 % 2)  # 1
+
+print(2 ** 3)  # 8
+print(c * 2)  # 11
+
+print(c + ' ' + str(a))  # '1 1'
+print(c, a, b)  # 1 1 1.0
+# '' or "" - doesn't matter
+# + or , - you don't want ' ' or do
+
+print(1, 2, 3, sep=', ', end='.')  # пишет '1, 2, 3.' sep отвечает за часть между элементами, end за конец строки
