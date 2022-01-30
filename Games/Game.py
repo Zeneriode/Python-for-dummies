3e51a54a6a8544b50d64d41345e4418b4ed960ad
Zener_085 <57841939+Zener085@users.noreply.github.com>
2022-01-30 19:07:40 +0300

    Добавили фон в игру
    Создали подкласс для класса Rect, чтобы рисовать задний фон у игры "Змейка"
    А также добавили несколько констант для игры (размеры экрана, размеры одного блока игры, время между обновлением кадров)

--- Games/Game.py
+++ Games/Game.py
@@ -1,46 +1,37 @@
-import pygame.key
-from pygame import *  # импортируем библиотеку pygame для создания игр
-from time import sleep
+class Car:
+    def __init__(self, color):
+        self.avg_speed = 100
+        self.audio = "rrrrr"
+        self.color = color
+
+    def increase_speed(self) -> None:
+        print("Скорость автомобиля сейчас:", end=" ")
+        for speed in range(0, self.avg_speed, 10):
+            print(str(speed) + "...", end=" ")
+        print()
+
+    def say_color(self) -> str:
+        return "Цвет машины: " + self.color
 
 
-init()  # просто создаём пустое поле
-# display.set_caption("Game")  # задаём название для поля
-screen = display.set_mode([800, 600])  # задаём размеры для поля
+# lamborghini = Car("Yellow")
+# lamborghini.increase_speed()
+# print(lamborghini.say_color())
+# print(lamborghini.color)
 
-red = (255, 0, 0)  # я для примера создал 3 переменные - 3 разных цвета по шкале RGB (Google в помощь, что это:) )
-green = (0, 255, 0)
-blue = (0, 0, 255)
+class Lamborghini(Car):
+    def __init__(self, color, display):
+        super().__init__(color)
+        self.display = display
+        self.avg_speed = 300
 
-img = image.load("../Module 3/snake_head.png")
-img_rect = img.get_rect()
-
-x = 100
-y = 100
-
-r = Rect(x, y, 50, 10)  # создадим объект - прямоугольник с определенными координатами и размерами
-draw.rect(screen, red, r)  # нарисуем этот прямоугольник на нашем поле
-screen.blit(img, (5, 5))
-
-
-# async def move():
-#     global x, y
-#     if key.key_code("d"):
-#         x = x + 1
-#         y = y + 1
-#     sleep(3)
+    def increase_speed(self) -> None:
+        print("Скорость автомобиля сейчас:", end=" ")
+        for speed in range(0, self.avg_speed, 50):
+            print(str(speed) + "...", end=" ")
+        print("Автомобиль приехал в пункт назначения.")
 
 
-play = True  # просто переменная, которая будет помогать фиксироваться цикл
-while play:  # создаём бесконечный цикл
-    for e in event.get():  # проверяем, что мы делаем на нашем поле
-        if e.type == QUIT:  # если мы нажали на крестик, то
-            play = False    # просто закрываем наше окно
-    draw.rect(screen, red, r)  # нарисуем этот прямоугольник на нашем поле
-    screen.blit(img, (5, 5))
-    r = Rect(x, y, 50, 50)  # создадим объект - прямоугольник с определенными координатами и размерами
-    # move()
-    # print(x, y)
-    # draw.rect(screen, "blue", Rect(0, 0, 800, 600))
-    display.update()  # обновляем поле - если появится что-то новое, то оно рисуется или изменится
-
-# sleep(3)  # если мы оставляем окно постоянно открытым, то хотя бы просто удержим его на 3 секунды
+lambo = Lamborghini("Green", "Ipad 5")
+lambo.increase_speed()
+print(lambo.say_color())
--- Games/example.py
+++ Games/example.py
@@ -1,14 +1,35 @@
 from pygame import *
 
-init()
+# Константы
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 700
+BLOCK_SIZE = 25
+DELAY = 60
 
-screen = display.set_mode((800, 600))
+init()  # запускаем файл, окно
+
+screen = display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # окно
+
+
+class Land(Rect):
+    """Задний фон игры"""
+    def __init__(self, *args, **kwds):
+        super().__init__(*args, **kwds)
+        self.background = "#FFFFFF"
+
+    def draw(self):
+        """Рисует задний фон в виде прямоугольника на все окно"""
+        global screen  # где мы будем рисовать наш фон
+        draw.rect(screen, self.background, [self.left, self.top, self.width, self.height])
+
+
+land = Land(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)  # Задний фон для игры
 
 play = True
 while play:
     for e in event.get():
-        print(e, e.type)
         if e.type == QUIT:
             play = False
 
+    land.draw()
     display.update()
