5be86e2e96305075e2a4ce6f09c0f0df55b8d2dd
Zener_085 <zener.085@mail.ru>
2022-04-25 00:41:59 +0300

    Начал разработку Discord бота
    Реализован минимальный бот, который может только отвечать на "Hello", а также система сохранения истории ошибок при запуске программы
    
    Дополнительно сохранил нескольк файлов для работы с R (в личных целях)

---
+++ Bots/Discord/recommend_logging.py
@@ -0,0 +1,11 @@
+﻿"""
+Библиотека discord использует библиотеку logging, чтобы фиксировать ошибки при запуске программы. Поэтому, чтобы читать
+их в нормальном виде, желательно использовать .log файл, который будет фиксировать и записывать историю всех ошибок.
+"""
+import logging
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
