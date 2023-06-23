6823a34af5ad2563dec8401765369b8a1f2b8c8a
Zener_085 <57841939+Zener085@users.noreply.github.com>
2023-06-23 18:27:58 +0300

    2 games

---
+++ en/Module_2/Practice/miniprojects.py
@@ -0,0 +1,20 @@
+"""
+Guess the number:
+There are 2 players: computer and user.
+One of them minds the number from a to b (a and b are natural numbers, can be specified), and another has to guess this
+number. Then they change their roles and repeat the whole process. The winner is one who makes fewer attempts to guess
+the number.
+Ideas to upgrade: Make functionality that players will answer like "bigger" or "smaller" that the second player can
+determine the position of the guessed number
+"""
+
+# The program
+
+"""
+Blackjack:
+There are 2 players, they play one after one.
+Each player takes 2 cards initially. If he wants, he can pick more cards.
+The task of the players is to collect in total 21 points. If there will be at least 22 points, then the player loses
+immediately. Otherwise the winner is one who picked more points.
+Ideas to upgrade: increase time of execution that computer will look like a human
+"""
